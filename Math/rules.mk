f6fc19e44578b1bbb4ef05d243c8c59f62dfefd5
juanchopanza <juan.palacios.puyana@gmail.com>
2012-07-12 07:44:28 +0200

    Add Makefiles and rules.mk.

---
+++ Math/rules.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2012 Juan Palacios juan.palacios.puyana@gmail.com
+# This file is part of minimath.
+# Subject to the Lesser GNU Public License 
+# - see < http://www.gnu.org/licenses/lgpl.html>
+#
+
+
+INCLUDEDIR = $(PWD)/include
+CPPFLAGS = -I$(INCLUDEDIR)
+CXXFLAGS = -Wall -Wextra -pedantic-errors -O2 -g
+CXX = /usr/bin/g++
